123df9406d8a7a6b0e40e16cbb760e58449d9da2
landonbuell <lhb1007@wildcats.unh.edu>
2020-10-20 20:57:20 -0400

    Added RealityCheck04 Solution

---
+++ RealityCheck04/RealityCheck04/Q1Main.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Landon Buell
+ * Mark Lyon
+ * MATH 753.01 - EC
+ * 20 october 2020
+ */
+
+using System;
+
+namespace RealityCheck04
+{
+    class Q1Main
+    {
+        static void Main()
+        {
+            // Establish Initial Positions & time Intervals
+            double[] _r0 = new double[4] { 0, 0, 6370, 0 };
+            double[] _t0 = new double[4] { 0.07074, 0.07220, 0.07690, 0.07242 };
+
+            // Create GPS System & Position the satellites
+            SystemGPS GPS = new SystemGPS(_r0,_t0);
+            GPS.Satellite1Position = new double[3] { 15600, 7540, 20140};
+            GPS.Satellite2Position = new double[3] { 18760, 2750, 18610 };
+            GPS.Satellite3Position = new double[3] { 17610, 14630, 13480 };
+            GPS.Satellite4Position = new double[3] { 19170, 610, 18390 };
+
+        }
+    }
+
+}
---
+++ RealityCheck04/RealityCheck04/Q1Utils.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealityCheck04
+{
+    class MultivariateNewtonsMethodSolver
+    {
+    }
+
+    public class SystemGPS
+    {
+        // Representas a GPS System of Equations
+        private readonly double c = 299792458;
+
+        public SystemGPS(double[] r1, double[] r2, double[] r3, double[] r4)
+        {
+            // Constructor Method for SystemGPS Instance
+            Satellite1Position = r1;
+            Satellite2Position = r2;
+            Satellite3Position = r3;
+            Satellite4Position = r4;
+        }
+
+        public SystemGPS(double[] _r0, double[] _t0)
+        {
+            // Constructor for different arguments
+            InitialPosition = _r0;
+            TimeIntervals = _t0;
+        }
+            
+        public double[] TimeIntervals { get; set; }
+
+        public double[] InitialPosition { get; set; }
+
+        public double[] Satellite1Position { get; set; }
+        public double[] Satellite2Position { get; set; }
+        public double[] Satellite3Position { get; set; }
+        public double[] Satellite4Position { get; set; }
+
+        public void SystemOfEquations(double _r0)
+        {
+            // Return value of system of equations
+        }
+
+    }
+}
