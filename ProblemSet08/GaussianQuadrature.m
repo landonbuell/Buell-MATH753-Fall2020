da650666d9b69a927d82f1aa68bc2848ebb8e70e
landonbuell <lhb1007@wildcats.unh.edu>
2020-10-29 23:17:21 -0400

    Update GaussianQuadrature.m

--- ProblemSet08/GaussianQuadrature.m
+++ ProblemSet08/GaussianQuadrature.m
@@ -1,12 +1,9 @@
-function [value] = GaussianQuadrature(func1,a,b)
-%GaussianQuadrature Integrate 'func1' on interval [a,b] using N steps
+function [value] = GaussianQuadrature(func)
+%GaussianQuadrature Integrate 'func' on interval [a,b] using N steps
 
 % Define Constants
 x = [-0.86113631159405,-0.33998104358486,0.33998104358486,0.86113631159405];
 c = [0.34785484513745,0.65214515486255,0.65214515486255,0.34785484513745];
-
-func2 = @(t) ((b-a)*t+b+a)/2;
-func = func1(func2);
 
 % Compute sum
 value = 0;
