9509224f8a46ce3843e635a39280587be4e302f1
landonbuell <lhb1007@wildcats.unh.edu>
2020-11-12 16:59:50 -0500

    Updated MATLAB Scripts

--- ProblemSet09/Q622_Computer.m
+++ ProblemSet09/Q622_Computer.m
@@ -1,5 +1,5 @@
 % Landon Buell
-% Q 6.1.2 - Computer Probs
+% Q 6.2.2 - Computer Probs
 % MATH 753.01
 % 10 Nov 2020
 
@@ -18,7 +18,7 @@ for i = 1:3
     ODE_A = @(t,y) t;
     Sol_A = @(t) 0.5 * t.^2 + 1;
     yA = Sol_A(t);
-    wA = EulerMethod(ODE_A,t,y0);
+    wA = MidpointMethod(ODE_A,t,y0);
     
     % Plot the Values 
     figure  
@@ -35,7 +35,7 @@ for i = 1:3
     ODE_C = @(t,y) 2*(t+1)*y;
     Sol_C = @(t) exp(t.^2 + 2*t );
     yC = Sol_C(t);
-    wC = EulerMethod(ODE_C,t,y0);
+    wC = MidpointMethod(ODE_C,t,y0);
     
     % Plot the Values 
     figure  
@@ -52,7 +52,7 @@ for i = 1:3
     ODE_E = @(t,y) 1/y^2;
     Sol_E = @(t) (3*t+1).^(1/3);
     yE = Sol_E(t);
-    wE = EulerMethod(ODE_E,t,y0);
+    wE = MidpointMethod(ODE_E,t,y0);
     
     % Plot the Values 
     figure  
@@ -65,3 +65,42 @@ for i = 1:3
     drawnow;
     
 end
+% Landon Buell
+% Q 6.1.2 - Computer Probs
+% MATH 753.01
+% 10 Nov 2020
+
+t0 = 0;
+tf = 1;
+h = 0.1;
+y0 = 1;
+t = [t0:h:tf];
+
+format long;
+
+%%%% Solve Part A %%%%
+disp("Part A");
+ODE_A = @(t,y) t;
+Sol_A = @(t) 0.5 * t.^2 + 1;
+yA = Sol_A(t);
+wA = RungeKutta4(ODE_A,t,y0);
+err_A = yA - wA; 
+TableA = [t' , yA' , wA' , err_A']
+
+%%%% Solve Part C %%%%
+disp("Part C");
+ODE_C = @(t,y) 2*(t+1)*y;
+Sol_C = @(t) exp(t.^2 + 2*t );
+yC = Sol_C(t);
+wC = RungeKutta4(ODE_C,t,y0);
+err_C = yC - wC; 
+TableC = [t' , yC' , wC' , err_C']
+
+%%%% Solve Part E %%%%
+disp("Part E");
+ODE_E = @(t,y) 1/y^2;
+Sol_E = @(t) (3*t+1).^(1/3);
+yE = Sol_E(t);
+wE = RungeKutta4(ODE_E,t,y0);
+err_E = yE - wE; 
+TableE = [t' , yE' , wE' , err_E']
